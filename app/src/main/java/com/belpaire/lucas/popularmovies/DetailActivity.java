f6362e4803372dd0deb8aec086c85bb9eeca6fdf
lucasbelpaire <lucas.belpaire@ugent.be>
2018-08-20 17:02:49 +0200

    Implemented DetailActivity.java

--- app/src/main/java/com/belpaire/lucas/popularmovies/DetailActivity.java
+++ app/src/main/java/com/belpaire/lucas/popularmovies/DetailActivity.java
@@ -1,4 +1,49 @@
 package com.belpaire.lucas.popularmovies;
 
-public class DetailActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.ShareCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private static final String MOVIE_SHARE_HASHTAG = " #PopularMoviesApp";
+
+    private String mMovieTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+        TextView mMovieDisplay = findViewById(R.id.tv_display_movie);
+
+        Intent intentThatStartedThisActivity = getIntent();
+
+        if(intentThatStartedThisActivity != null){
+            if (intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)){
+                mMovieTitle = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+                mMovieDisplay.setText(mMovieTitle);
+            }
+        }
+    }
+
+    private Intent createShareMovieIntent() {
+        return ShareCompat.IntentBuilder.from(this)
+                .setType("text/plain")
+                .setText(mMovieTitle + MOVIE_SHARE_HASHTAG)
+                .getIntent();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.detail, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_share);
+        menuItem.setIntent(createShareMovieIntent());
+        return true;
+    }
+
 }
