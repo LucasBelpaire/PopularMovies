75ca5866fbf8c67afadece0e6f942582dffcb202
lucasbelpaire <lucas.belpaire@ugent.be>
2018-08-20 21:33:44 +0200

    Minor update.

--- app/src/main/java/com/belpaire/lucas/popularmovies/utilities/OpenMovieDataJsonUtils.java
+++ app/src/main/java/com/belpaire/lucas/popularmovies/utilities/OpenMovieDataJsonUtils.java
@@ -2,9 +2,11 @@ package com.belpaire.lucas.popularmovies.utilities;
 
 import android.content.Context;
 
+import org.json.JSONException;
+
 public final class OpenMovieDataJsonUtils {
 
-    public static String[] getSimpleMovieDataStringsFromJson(Context context, String jsonMovieDataStr){
+    public static String[] getSimpleMovieDataStringsFromJson(Context context, String jsonMovieDataStr) throws JSONException{
 
     }
 }
